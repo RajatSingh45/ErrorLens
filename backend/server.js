a25624f4e2fc7f4777cd930945b3ad51db6f16c8
RajatSingh45 <rs738489@gmail.com>
2026-03-26 00:33:29 +0530

    Have Created the RabbitMQ connection and channel, alos created the queue called error_queue, and created one file that fetch the errors from outbox and send it to queue

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+import app from "./src/app.js";
+import { connectQueue } from "./src/config/rabbitmq.js";
+import processOutbox from "./src/jobs/outbox.processor.js";
 
-import app from './src/app.js';
+const PORT = process.env.PORT || 5000;
 
-const PORT=process.env.PORT||5000;
+await connectQueue();
 
+setInterval(()=>{
+    processOutbox();
+},5000);
 
-app.listen(PORT,()=>{
- console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
-
