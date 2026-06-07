ba8c2f12502c7887724be45d0c3bd4d4a21ab2cd
RajatSingh45 <rs738489@gmail.com>
2026-06-07 23:00:58 +0530

    Deployed RabbiMq and Redis

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,14 +1,31 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import Redis from 'ioredis'
+// import dotenv from 'dotenv'
+// dotenv.config()
+// import Redis from 'ioredis'
 
-const redis=new Redis({
-    host:process.env.REDIS_HOST,
-    port:process.env.REDIS_PORT
+// const redis=new Redis({
+//     host:process.env.REDIS_HOST,
+//     port:process.env.REDIS_PORT
+// });
+
+// redis.on("connect",()=>{
+//     console.log("Redis connected successfully");
+// })
+
+// export default redis;
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import Redis from "ioredis";
+
+const redis = new Redis(process.env.REDIS_URL);
+
+redis.on("connect", () => {
+  console.log("Redis connected successfully");
 });
 
-redis.on("connect",()=>{
-    console.log("Redis connected successfully");
-})
+redis.on("error", (err) => {
+  console.error("Redis error:", err.message);
+});
 
 export default redis;
